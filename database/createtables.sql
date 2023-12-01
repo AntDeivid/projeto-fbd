32692d435ba480fdcde8c0299f36b34a37ef8627
Antonio Deivid <antonio.costa@nepen.org.br>
2023-11-30 21:18:52 -0300

    add correct create-table script

--- database/createtables.sql
+++ database/createtables.sql
@@ -1,39 +1,66 @@
-CREATE TABLE Usuario
+CREATE TABLE Usuário
 (
   Nome VARCHAR NOT NULL,
   Dia INT NOT NULL,
-  Mes INT NOT NULL,
+  Mês INT NOT NULL,
   Ano INT NOT NULL,
   Idade INT NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
-  Numero INT NOT NULL,
-  Endereco VARCHAR NOT NULL,
-  Tipo_sanguineo VARCHAR NOT NULL,
-  Historico_medico VARCHAR NOT NULL,
+  CpfUsuario INT NOT NULL,
+  Número INT NOT NULL,
+  Endereço VARCHAR NOT NULL,
+  Tipo_sanguíneo VARCHAR NOT NULL,
+  Histórico_médico VARCHAR NOT NULL,
   Peso FLOAT NOT NULL,
   PRIMARY KEY (CpfUsuario),
-  UNIQUE (Numero)
+  UNIQUE (Número)
+);
+
+CREATE TABLE Atividade_Física
+(
+  Tempo INT NOT NULL,
+  Tipo VARCHAR NOT NULL,
+  Objetivo VARCHAR NOT NULL,
+  AtividadeId INT NOT NULL,
+  CpfUsuario INT NOT NULL,
+  PRIMARY KEY (AtividadeId),
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario)
 );
 
 CREATE TABLE Dieta
 (
-  Meta_Calorica FLOAT NOT NULL,
-  Quantidade_de_refeicoes INT NOT NULL,
-  Intervalo_entre_refeicoes VARCHAR NOT NULL,
+  Meta_Calórica INT NOT NULL,
+  Quantidade_de_refeições INT NOT NULL,
+  Intervalo_entre_refeições INT NOT NULL,
   Objetivo VARCHAR NOT NULL,
   DietaId INT NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
+  CpfUsuario INT NOT NULL,
   PRIMARY KEY (DietaId),
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario)
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario)
 );
 
-CREATE TABLE Registro_Medico
+CREATE TABLE Medicação
+(
+  Nome VARCHAR NOT NULL,
+  Dosagem VARCHAR NOT NULL,
+  Tipo VARCHAR NOT NULL,
+  Intervalo_ INT NOT NULL,
+  MedicacaoId INT NOT NULL,
+  CpfUsuario INT NOT NULL,
+  PRIMARY KEY (MedicacaoId),
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario)
+);
+
+CREATE TABLE Registro_Médico
 (
   RegistroId INT NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
+  CpfUsuario INT NOT NULL,
+  AtividadeId INT NOT NULL,
+  MedicacaoId INT NOT NULL,
   DietaId INT NOT NULL,
   PRIMARY KEY (RegistroId),
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario),
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario),
+  FOREIGN KEY (AtividadeId) REFERENCES Atividade_Física(AtividadeId),
+  FOREIGN KEY (MedicacaoId) REFERENCES Medicação(MedicacaoId),
   FOREIGN KEY (DietaId) REFERENCES Dieta(DietaId)
 );
 
@@ -41,82 +68,61 @@ CREATE TABLE Exame
 (
   Tipo VARCHAR NOT NULL,
   Dia INT NOT NULL,
-  Mes INT NOT NULL,
+  Mês INT NOT NULL,
   Ano INT NOT NULL,
   Nome VARCHAR NOT NULL,
   ExameId INT NOT NULL,
   Finalidade VARCHAR NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
+  CpfUsuario INT NOT NULL,
   PRIMARY KEY (ExameId),
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario)
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario)
 );
 
-CREATE TABLE Usuario_Parentesco
+CREATE TABLE Usuário_Parentesco
 (
   Parentesco VARCHAR NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario)
+  CpfUsuario INT NOT NULL,
+  PRIMARY KEY (CpfUsuario),
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario)
 );
 
 CREATE TABLE Dieta_Alimentos
 (
   Alimentos VARCHAR NOT NULL,
   DietaId INT NOT NULL,
+  PRIMARY KEY (DietaId),
   FOREIGN KEY (DietaId) REFERENCES Dieta(DietaId)
 );
 
-CREATE TABLE Registro_Medico_Possiveis_Doencas
+CREATE TABLE Medicação_Contraindicação
 (
-  Possiveis_Doencas_ VARCHAR NOT NULL,
-  RegistroId INT NOT NULL,
-  FOREIGN KEY (RegistroId) REFERENCES Registro_Medico(RegistroId)
-);
-
-CREATE TABLE Usuario_Reponsavel
-(
-  CpfUsuario VARCHAR NOT NULL,
-  RegistroId INT NOT NULL,
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario),
-  FOREIGN KEY (RegistroId) REFERENCES Registro_Medico(RegistroId)
-);
-
-CREATE TABLE Atividade_Fisica
-(
-  Tempo VARCHAR NOT NULL,
-  Tipo VARCHAR NOT NULL,
-  Objetivo VARCHAR NOT NULL,
-  AtividadeId INT NOT NULL,
-  RegistroId INT NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
-  PRIMARY KEY (AtividadeId),
-  FOREIGN KEY (RegistroId) REFERENCES Registro_Medico(RegistroId),
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario)
-);
-
-CREATE TABLE Medicacao
-(
-  Nome VARCHAR NOT NULL,
-  Dosagem FLOAT NOT NULL,
-  Tipo VARCHAR NOT NULL,
-  Intervalo_ VARCHAR NOT NULL,
-  MedicacaoId VARCHAR NOT NULL,
-  RegistroId INT NOT NULL,
-  CpfUsuario VARCHAR NOT NULL,
+  Contraindicação VARCHAR NOT NULL,
+  MedicacaoId INT NOT NULL,
   PRIMARY KEY (MedicacaoId),
-  FOREIGN KEY (RegistroId) REFERENCES Registro_Medico(RegistroId),
-  FOREIGN KEY (CpfUsuario) REFERENCES Usuario(CpfUsuario)
+  FOREIGN KEY (MedicacaoId) REFERENCES Medicação(MedicacaoId)
 );
 
-CREATE TABLE Medicacao_Contraindicacao
+CREATE TABLE Medicação_Efeitos_Colateráis_
 (
-  Contraindicacao VARCHAR NOT NULL,
-  MedicacaoId VARCHAR NOT NULL,
-  FOREIGN KEY (MedicacaoId) REFERENCES Medicacao(MedicacaoId)
+  Efeitos_Colateráis_ VARCHAR NOT NULL,
+  MedicacaoId INT NOT NULL,
+  PRIMARY KEY (MedicacaoId),
+  FOREIGN KEY (MedicacaoId) REFERENCES Medicação(MedicacaoId)
 );
 
-CREATE TABLE Medicacao_Efeitos_Colaterais
+CREATE TABLE Registro_Médico_Possíveis_Doenças_
 (
-  Efeitos_Colaterais_ VARCHAR NOT NULL,
-  MedicacaoId VARCHAR NOT NULL,
-  FOREIGN KEY (MedicacaoId) REFERENCES Medicacao(MedicacaoId)
+  Possíveis_Doenças_ NUMERIC NOT NULL,
+  RegistroId INT NOT NULL,
+  PRIMARY KEY (RegistroId),
+  FOREIGN KEY (RegistroId) REFERENCES Registro_Médico(RegistroId)
+);
+
+CREATE TABLE Usuário_Reponsável
+(
+  CpfUsuario INT NOT NULL,
+  RegistroId INT NOT NULL,
+  PRIMARY KEY (CpfUsuario),
+  FOREIGN KEY (CpfUsuario) REFERENCES Usuário(CpfUsuario),
+  FOREIGN KEY (RegistroId) REFERENCES Registro_Médico(RegistroId)
 );
